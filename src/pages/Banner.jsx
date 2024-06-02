98985ebf8fa5be91d4a39ef551d450c5ef3e4641
MukterHossain <mukterhossain3075@gmail.com>
2024-06-02 16:27:17 +0600

    added banner section

--- src/pages/Banner.jsx
+++ src/pages/Banner.jsx
@@ -3,13 +3,24 @@
 const Banner = () => {
     return (
         <div>
-            <div className="hero min-h-screen" style={{ backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)' }}>
-                <div className="hero-overlay bg-opacity-60"></div>
-                <div className="hero-content text-center text-neutral-content">
-                    <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-                        <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                        <button className="btn btn-primary">Get Started</button>
+            <div className="hero min-h-screen rounded-sm my-12" style={{ backgroundImage: 'url(https://i.ibb.co/YLrgyqr/banner.jpg)' }}>
+                <div className="hero-overlay bg-opacity-60 rounded-sm"></div>
+                <div className=" ">
+                    <div className="text-center">
+                        <div className="text-white w-4/5 md:w-2/3   m-auto text-center ">
+                            <h1 className="mb-5 text-5xl font-bold">Welcome to DOE</h1>
+                            <p className="mb-5 text-center">The courier service industry uses platforms to manage their fleets, clients, and business operations. Some of these substrates are old and are no longer as effective as newer technologies, posing competition issues.</p>
+                        </div>
+                        <div className="">
+                            <div className="  w-1/2 m-auto flex  ">
+                                <div className=" w-2/3 bg-white rounded-l-xl">
+                                    <input type="text" placeholder="Type here" className="text-lg rounded-l-xl  outline-none px-3 py-2" />
+                                </div>
+                                <div className="border-l-2 w-1/3 rounded-r-xl bg-green-500">
+                                    <span className="text-center rounded-r-xl  px-3 py-2 text-xl ml-4">Search</span>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
